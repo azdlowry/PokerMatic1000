0501c0cf6144592feec1ec3b5f5db65321b92777
Simon <simonsmith.uk@gmail.com>
2014-10-22 10:34:16 +0100

    added enums

---
+++ Pokermatic1000.Tests/Convertors/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pokermatic1000.App;
+
+namespace Pokermatic1000.Tests.Convertors
+{
+    [TestClass]
+    public class ConvertorTest
+    {
+        [TestMethod]
+        public void Can_be_constructed()
+        {
+            var convertor = new Convertor();
+            Assert.IsNotNull(convertor);
+        }
+
+        [TestMethod]
+        public void converts_fold_to_OPPONENT_MOVE()
+        {
+            var inputs = new[] { "fold", "fold ", " fold", "Fold", "FOLD" };
+
+            foreach (var input in inputs)
+            {
+                var result = new Convertor().ConvertToOpponentMove(input);
+                Assert.AreEqual(OpponentMove.Fold, result, "failed using " + input);
+            }
+        }
+
+        [TestMethod]
+        public void converts_call_to_OPPONENT_MOVE()
+        {
+            var inputs = new[] { "call", "call ", " call", "CALL" };
+
+            foreach (var input in inputs)
+            {
+                var result = new Convertor().ConvertToOpponentMove(input);
+                Assert.AreEqual(OpponentMove.Call, result, "failed using " + input);
+            }
+        }
+
+        [TestMethod]
+        public void converts_bet_to_OPPONENT_MOVE()
+        {
+            var inputs = new[] { "bet", "bet   ", " BeT", "BET" };
+
+            foreach (var input in inputs)
+            {
+                var result = new Convertor().ConvertToOpponentMove(input);
+                Assert.AreEqual(OpponentMove.Bet, result, "failed using " + input);
+            }
+        }
+
+        
+    }
+}
---
+++ Pokermatic1000/App/Convertor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pokermatic1000.App
+{
+    public enum OpponentMove
+    {
+        Unknown = 0 ,
+        Fold  = 1 ,
+        Call = 2 ,
+        Bet = 3 
+    }
+
+    public enum Card
+    {
+        Unknown = 0,
+        C2 = 2,
+        C3 = 3,
+        C4 = 4,
+        C5 = 5,
+        C6 = 6,
+        C7= 7,
+        C8 = 8,
+        C9 = 8,
+        CT = 10,
+        CJ = 11,
+        CQ = 12,
+        CK = 13,
+        CA = 14	
+    }
+
+    public sealed class Convertor
+    {
+        public OpponentMove ConvertToOpponentMove(string input)
+        {
+            var ptr = input.ToLower().Trim().ToCharArray().First();
+            if (ptr == 'f')
+            {
+                return OpponentMove.Fold;
+            }
+
+            if (ptr == 'c')
+            {
+                return OpponentMove.Call;
+            }
+            
+            if (ptr == 'b')
+            {
+                return OpponentMove.Bet;
+            }
+            return  OpponentMove.Unknown;
+        }
+    }
+}
