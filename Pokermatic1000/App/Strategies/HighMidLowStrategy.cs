309602a636b25ef036fce81b4f02e3d378836def
AndyLowryLateRooms <andrew.lowry@laterooms.com>
2014-10-22 11:37:26 +0100

    Change to selecting sub strategy at game start

--- Pokermatic1000/App/Strategies/HighMidLowStrategy.cs
+++ Pokermatic1000/App/Strategies/HighMidLowStrategy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
@@ -9,28 +10,37 @@ namespace Pokermatic1000.App.Strategies
     {
         private readonly Card _ourCard;
         private bool hasBet = false;
+        private Func<OpponentMove> _subStrategy;
 
         public HighMidLowStrategy(Card ourCard)
         {
             _ourCard = ourCard;
+
+            if (_ourCard > Card.CT)
+            {
+                Trace.TraceWarning("Selecting High Strategy");
+                _subStrategy = () =>
+                    {
+                        var nextMove = hasBet ? OpponentMove.Call : OpponentMove.Bet;
+                        hasBet = true;
+                        return nextMove;
+                    };
+            }
+            else if (_ourCard > Card.C3)
+            {
+                Trace.TraceWarning("Selecting Mid Strategy");
+                _subStrategy = () => OpponentMove.Call;
+            }
+            else
+            {
+                Trace.TraceWarning("Selecting Low Strategy");
+                _subStrategy = () => OpponentMove.Fold;
+            }
         }
 
         public OpponentMove Move()
         {
-            if (_ourCard > Card.CT)
-            {
-                var nextMove = hasBet ? OpponentMove.Call : OpponentMove.Bet;
-                hasBet = true;
-                return nextMove;
-            }
-            else if (_ourCard > Card.C3)
-            {
-                return OpponentMove.Call;
-            }
-            else
-            {
-                return OpponentMove.Fold;
-            }
+            return _subStrategy();
         }
     }
 }
