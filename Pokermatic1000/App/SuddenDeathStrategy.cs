3da9b3b5cd740db5d1a55dc9d500e87d3bbfd5fc
AndyLowryLateRooms <andrew.lowry@laterooms.com>
2014-10-22 16:39:03 +0100

    SuddenDeathStrategy logging

--- Pokermatic1000/App/SuddenDeathStrategy.cs
+++ Pokermatic1000/App/SuddenDeathStrategy.cs
@@ -1,9 +1,16 @@
-﻿using Pokermatic1000.App.Strategies;
+﻿using System.Diagnostics;
+using Pokermatic1000.App.Strategies;
 
 namespace Pokermatic1000.App
 {
     public class SuddenDeathStrategy : IStrategy
     {
+        public SuddenDeathStrategy()
+        {
+
+            Trace.TraceWarning("Selecting SuddenDeathStrategy");
+        }
+
         public OpponentMove Move()
         {
             return OpponentMove.Fold;
