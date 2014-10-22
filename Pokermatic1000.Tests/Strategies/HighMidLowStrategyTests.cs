bd46e08671699daaf0602c1cee640aed3073d03a
AndyLowryLateRooms <andrew.lowry@laterooms.com>
2014-10-22 11:31:45 +0100

    Add HighMidLowStrategy

---
+++ Pokermatic1000.Tests/Strategies/HighMidLowStrategyTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pokermatic1000.App;
+using Pokermatic1000.App.Strategies;
+
+namespace Pokermatic1000.Tests.Strategies
+{
+    [TestClass]
+    public class HighMidLowStrategyTests
+    {
+        [TestMethod]
+        public void HighCard_BetsOnceThenCalls()
+        {
+            var strategy = new HighMidLowStrategy(Card.CA);
+
+            Assert.AreEqual(strategy.Move(), OpponentMove.Bet);
+            Assert.AreEqual(strategy.Move(), OpponentMove.Call);
+        }
+
+        [TestMethod]
+        public void MidCard_Calls()
+        {
+            var strategy = new HighMidLowStrategy(Card.C7);
+
+            Assert.AreEqual(strategy.Move(), OpponentMove.Call);
+        }
+
+        [TestMethod]
+        public void LowCard_Folds()
+        {
+            var strategy = new HighMidLowStrategy(Card.C3);
+
+            Assert.AreEqual(strategy.Move(), OpponentMove.Fold);
+        }
+    }
+}
---
+++ Pokermatic1000/App/Strategies/HighMidLowStrategy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pokermatic1000.App.Strategies
+{
+    public class HighMidLowStrategy
+    {
+        private readonly Card _ourCard;
+        private bool hasBet = false;
+
+        public HighMidLowStrategy(Card ourCard)
+        {
+            _ourCard = ourCard;
+        }
+
+        public OpponentMove Move()
+        {
+            if (_ourCard > Card.CT)
+            {
+                var nextMove = hasBet ? OpponentMove.Call : OpponentMove.Bet;
+                hasBet = true;
+                return nextMove;
+            }
+            else if (_ourCard > Card.C3)
+            {
+                return OpponentMove.Call;
+            }
+            else
+            {
+                return OpponentMove.Fold;
+            }
+        }
+    }
+}
