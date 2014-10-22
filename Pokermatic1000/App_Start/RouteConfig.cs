2bec90f3af8e44c2f21f1f5e7bc85fe19ddd07d6
Simon <simonsmith.uk@gmail.com>
2014-10-22 10:06:03 +0100

    added routing

--- Pokermatic1000/App_Start/RouteConfig.cs
+++ Pokermatic1000/App_Start/RouteConfig.cs
@@ -13,11 +13,19 @@ namespace Pokermatic1000
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+           
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "Default_one",
+                url: "{action}/",
+                defaults: new { controller = "Home", action = "Index"}
             );
+
+            routes.MapRoute(
+               name: "Default",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+           );
         }
     }
 }
